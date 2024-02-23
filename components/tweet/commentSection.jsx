b344a78b00b537e0c5bcccd86d259859fa34f24d
Somkene Ojukwu <somkeneoj@gmail.com>
2024-02-23 16:09:56 +0100

    working on comment profile pic design

--- components/tweet/commentSection.jsx
+++ components/tweet/commentSection.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import Image from "next/image";
 import { useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
